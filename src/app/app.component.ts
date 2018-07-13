b3e17f6598d211f6f8616191950e3be9f6946f91
Gustavo-PC\Gustavo <gustavo0326@gmail.com>
2018-07-13 01:30:10 -0500

    bin and bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  property = 'desde un parametro ';
   title = 'app';
+  fromEvent ='';
+
+  constructor() { 
+    console.log("constructor display1");
+    
+}
+  
+  ngOnInit() {
+    console.log("ngOnInit display1");
+  }
+
+  TextfromFunction() {
+    this.property ='desde una funciòn ';
+  }
+  onUpdateFromEvent(event:any ){
+    this.fromEvent = event.target.value;
+    console.log(event);
+    console.log(event.currentTarget.dataset.name);
+  }
+
 }
--- src/app/pantalla2/pantalla2.component.ts
+++ src/app/pantalla2/pantalla2.component.ts
@@ -5,11 +5,44 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pantalla2.component.html',
   styleUrls: ['./pantalla2.component.css']
 })
+
 export class Pantalla2Component implements OnInit {
-
-  constructor() { }
-
+  index=1;
+  url="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Naruto_logo.svg/1200px-Naruto_logo.svg.png"
+  anime ='Naruto';
+  trama ='En ese punto Sasuke intenta matar a los 5 kages, argumentando que desea formar un nuevo mundo sin el sistema ninja dando a conocer que lo que desea es una revolución, es vencido por Naruto en la batalla final de la historia, después de esto y con Kakashi como el Sexto Hokage';
+  
+  constructor() { 
+    console.log("constructor");
+    
+}
+  
   ngOnInit() {
+    console.log("ngOnInit");
   }
+
+  OnChanges(){
+    console.log("OnChanges");
+  }
+  ngDoCheck(){
+    //console.log("ngDoCheck");
+    if (this.url =="https://vignette.wikia.nocookie.net/gamefactory/images/2/25/Naruto-Shippuden-logo.png/revision/latest?cb=20151019075228") {
+      setTimeout(() => {
+      this.url ="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Naruto_logo.svg/1200px-Naruto_logo.svg.png";
+      },6000);
+    }else{
+      setTimeout(() => {
+        this.url ="https://vignette.wikia.nocookie.net/gamefactory/images/2/25/Naruto-Shippuden-logo.png/revision/latest?cb=20151019075228";
+        },6000);
+    }
+    
+  }
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit()");
+  }
+  ngAfterContentInit(){
+    console.log("ngAfterContentInit");
+  }
+  
 
 }
